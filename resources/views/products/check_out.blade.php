60c2fa0456e59aabb4eea5a0abdad4d7c9aebf60
MD MOMINUL ISLAM <mominbd532@gmail.com>
2020-04-19 22:36:14 +0600

    Make delivery address table  and  copy billing address to shopping address by click button on checkout page

--- resources/views/products/check_out.blade.php
+++ resources/views/products/check_out.blade.php
@@ -37,8 +37,8 @@
                                 <input type="text" class="form-control" name="billing_mobile" id="billing_mobile" value="{{$user_details->mobile}}"  placeholder="Billing Mobile">
                             </div>
                             <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="billtoship">
-                                <label class="form-check-label" for="billtoship">Shipping Address same as Billing Address</label>
+                                <input type="checkbox" class="form-check-input" id="billToShip">
+                                <label class="form-check-label" for="billToShip">Shipping Address same as Billing Address</label>
                             </div>
 
 
@@ -52,28 +52,33 @@
 
                             <h2>Shipping To</h2>
                             <div class="form-group">
-                                <input type="text" class="form-control" id=""  placeholder="Shipping Name">
+                                <input type="text" class="form-control" name="shipping_name" id="shipping_name"  placeholder="Shipping Name">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id=""  placeholder="Shipping Address">
+                                <input type="text" class="form-control" name="shipping_address" id="shipping_address"  placeholder="Shipping Address">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id=""  placeholder="Shipping City">
+                                <input type="text" class="form-control" name="shipping_city" id="shipping_city"  placeholder="Shipping City">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id=""  placeholder="Shipping State">
+                                <input type="text" class="form-control" name="shipping_state" id="shipping_state"  placeholder="Shipping State">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id=""  placeholder="Shipping Country">
+                                <select class="form-control" name="shipping_country" id="shipping_country" >
+                                    <option value="">Select Country</option>
+                                    @foreach($countries as $country)
+                                        <option value="{{$country->country_name}}"> {{$country->country_name}} </option>
+                                    @endforeach
+                                </select>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id=""  placeholder="Shipping Pincode">
+                                <input type="text" class="form-control" name="shipping_pincode" id="shipping_pincode"  placeholder="Shipping Pincode">
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id=""  placeholder="Shipping Phone">
+                                <input type="text" class="form-control" name="shipping_mobile" id="shipping_mobile"  placeholder="Shipping Mobile">
                             </div>
                             <div class="form-group">
-                                <input type="submit" class="form-control" id=""  placeholder="Shipping Phone">
+                                <input type="submit" class="form-control" value="Next">
                             </div>
 
                         </div><!--/sign up form-->
